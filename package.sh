6e53465322201126f348fe71bb17a22258bf29bd
bdimmick <me@billdimmick.com>
2024-11-05 14:44:24 -0800

    adding comments to packaging script

--- package.sh
+++ package.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
+# Clean up from previous failed runs.
 rm -rf ./package
 rm -rf ./lambda_function.zip
 
+# Install dependencies
 pip install -r requirements.txt --target ./package --trusted-host pypi.python.org
+
+# Package up Lambda
 cd package
 zip ../lambda_function.zip *
 cd ..
-# rm -rf ./package
+rm -rf ./package
 zip lambda_function.zip lambda_function.py
 sleep 5
