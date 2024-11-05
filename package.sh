ac5ba1dc01d223d01ec8bb68bb1fa88021938729
bdimmick <me@billdimmick.com>
2024-11-05 15:29:13 -0800

    updating package script

--- package.sh
+++ package.sh
@@ -3,14 +3,13 @@
 # Clean up from previous failed runs.
 rm -rf ./package
 rm -rf ./lambda_function.zip
+mkdir ./package
 
 # Install dependencies
 pip install -r requirements.txt --target ./package --trusted-host pypi.python.org
-
 # Package up Lambda
 cd package
-zip ../lambda_function.zip *
+zip -r ../lambda_function.zip *
 cd ..
 rm -rf ./package
-zip lambda_function.zip lambda_function.py
-sleep 5
+zip -u lambda_function.zip lambda_function.py
